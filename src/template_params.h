c6f77e171b3e01291526ea12c06ca5bed5a55164
Nobuo Hayashi <haya.biz@gmail.com>
2024-08-10 23:53:03 +0900

    feat: Update template parameters for WiFi and IoT channel
    This commit updates the template parameters for WiFi credentials and IoT channel information. It allows users to easily customize the SSID, password, channel ID, and write key for their specific setup. This improves the flexibility and reusability of the code.

--- src/template_params.h
+++ src/template_params.h
@@ -1,7 +1,9 @@
-
+// This file is used to store the parameters of the project
+// You can modify the parameters according to your needs
+// Save this file as src/params.h and modify the parameters
 const char* ssid = "YourSSID";
 const char* password = "YourPassword";
 
-unsigned int channelId = 12345; //YourChannelID;
+unsigned int channelId = 12345; //Your Ambient ChannelID;
 const char* writeKey = "YourWriteKey";
 
